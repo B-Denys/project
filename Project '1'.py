4cb3358200dc9467ecd7a4a1078508570a205048
Денис <butenko.denis132@gmail.com>
2024-09-12 18:02:55 +0300

    File

--- Project '1'.py
+++ Project '1'.py
@@ -2,7 +2,7 @@ Greeting = "Hello World"
 Family_Name = "Бутенко"
 Name = "Денис"
 Age = 16
-print(f"Змінна Greeting: Тип - {type(Greeting)}, Значення: {Greeting}")
-print(f"Змінна Family_Name: Тип - {type(Family_Name)}, Значення: {Family_Name}")
-print(f"Змінна Name: Тип - {type(Name)}, Значення: {Name}")
-print(f"Змінна Age: Тип - {type(Age)}, Значення: {Age}")
+print("Змінна Greeting: Тип -",type(Greeting),"Значення:",Greeting)
+print("Змінна Family_Name: Тип -",type(Family_Name),"Значення:",Family_Name)
+print("Змінна Name: Тип -",type(Name),"Значення:",Name)
+print("Змінна Age: Тип -",type(Age),"Значення:",Age)
--- Project '2'.py
+++ Project '2'.py
@@ -1,1 +1,9 @@
-print("Hello World")
+from collections import Counter
+Greeting = "Hello World"
+Name = "Денис"
+Number = 1
+Types_List = [type(Greeting), type(Name), type(Number)]
+Use_Types_List = [type(StrInt) for StrInt in Types_List]
+type_counts = Counter(Use_Types_List)
+Most_Type = type_counts.most_common(1)[0][0]
+print("Найчастіше зустрічається тип даних:",Most_Type)
