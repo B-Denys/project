0d83262f1f5a72d17ca527e02aa62ab4250a7690
Денис <butenko.denis132@gmail.com>
2024-09-12 14:39:32 +0300

    File

---
+++ Project.py
@@ -0,0 +1,8 @@
+Greeting = "Hello World"
+Family_Name = "Бутенко"
+Name = "Денис"
+Age = 16
+print(f"Змінна Greeting: Тип - {type(Greeting)}, Значення: {Greeting}")
+print(f"Змінна Family_Name: Тип - {type(Family_Name)}, Значення: {Family_Name}")
+print(f"Змінна Name: Тип - {type(Name)}, Значення: {Name}")
+print(f"Змінна Age: Тип - {type(Age)}, Значення: {Age}")
