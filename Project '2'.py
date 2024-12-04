b477021e3ac43fd2cfc7966267b6dc1dd56070c5
Денис <butenko.denis132@gmail.com>
2024-12-04 08:15:12 +0300

    File

--- Project '2'.py
+++ Project '2'.py
@@ -4,6 +4,6 @@ Name = "Денис"
 Number = 1
 Types_List = [type(Greeting), type(Name), type(Number)]
 Use_Types_List = [type(StrInt) for StrInt in Types_List]
-type_counts = Counter(Use_Types_List)
-Most_Type = type_counts.most_common(1)[0][0]
+Type_counts = Counter(Use_Types_List)
+Most_Type = Type_counts.most_common(1)[0][0]
 print("Найчастіше зустрічається тип даних:",Most_Type)
