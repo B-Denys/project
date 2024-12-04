c05f115901719249da1f238de328c894fe2a5b3e
Денис <butenko.denis132@gmail.com>
2024-12-04 08:13:57 +0300

    File

--- Project '3'.py
+++ Project '3'.py
@@ -1,7 +1,7 @@
-inner_dict = {"звичайній_ключ1": "Hello World", "звичайній_ключ2": 1.2, "звичайній_ключ3": [1, 2, 3], "звичайній_ключ4": True, "звичайній_ключ5": None}
-main_dict = {"ключ1": "Hello World", "ключ2": 12, "ключ3": inner_dict, "ключ4": [10, 20, 30]}
-type_dict = {"ключ1": type(main_dict["ключ1"]), "ключ2": type(main_dict["ключ2"]), "ключ3": type(main_dict["ключ3"]), "ключ4": type(main_dict["ключ4"])}
-inner_type_dict = {"звичайній_ключ1": type(inner_dict["звичайній_ключ1"]), "звичайній_ключ2": type(inner_dict["звичайній_ключ2"]), "звичайній_ключ3": type(inner_dict["звичайній_ключ3"]), "звичайній_ключ4": type(inner_dict["звичайній_ключ4"]), "звичайній_ключ5": type(inner_dict["звичайній_ключ5"])}
-print("Словник з типами даних:", type_dict)
-print("Звичайній словник:", main_dict)
-print("Словник з типами даних звичайного словника:", inner_type_dict)
+Ordinary_dict = {"звичайній_ключ1": "Hello World", "звичайній_ключ2": 1.2, "звичайній_ключ3": [1, 2, 3], "звичайній_ключ4": True, "звичайній_ключ5": None}
+Main_dict = {"ключ1": "Hello World", "ключ2": 12, "ключ3": Ordinary_dict, "ключ4": [10, 20, 30]}
+Type_dict = {"ключ1": type(Main_dict["ключ1"]), "ключ2": type(Main_dict["ключ2"]), "ключ3": type(Main_dict["ключ3"]), "ключ4": type(Main_dict["ключ4"])}
+Ordinary_type_dict = {"звичайній_ключ1": type(Ordinary_dict["звичайній_ключ1"]), "звичайній_ключ2": type(Ordinary_dict["звичайній_ключ2"]), "звичайній_ключ3": type(Ordinary_dict["звичайній_ключ3"]), "звичайній_ключ4": type(Ordinary_dict["звичайній_ключ4"]), "звичайній_ключ5": type(Ordinary_dict["звичайній_ключ5"])}
+print("Словник з типами даних:", Type_dict)
+print("Звичайній словник:", Main_dict)
+print("Словник з типами даних звичайного словника:", Ordinary_type_dict)
